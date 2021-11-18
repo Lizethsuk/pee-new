497e6fe4a381888bfe3ab0a75204d081bd850c99
Sandra <sandrarocio12oc@gmail.com>
2021-11-18 18:41:18 -0500

    add changes

--- js/data.js
+++ js/data.js
@@ -1,66 +1,66 @@
-const db = firebase.firestore();
-const auth = firebase.auth();
+// const db = firebase.firestore();
+// const auth = firebase.auth();
 
-new Vue({
-  data() {
-    return {
-      name: '',
-      usuarios: [],
-      countOfPage: 10,
-      currPage: 1,
-      filter_name: ''
-    }
-  },
-  computed: {
-    // filteredRows: function () {
-    //   var filter_name = this.filter_name.toLowerCase();
-    //   return (this.filter_name.trim() !== '') ?
-    //     this.rows.filter(function (d) { return d.name.toLowerCase().indexOf(filter_name) > -1; }) :
-    //     this.rows;
-    // },
+// new Vue({
+//   data() {
+//     return {
+//       name: '',
+//       usuarios: [],
+//       countOfPage: 10,
+//       currPage: 1,
+//       filter_name: ''
+//     }
+//   },
+//   computed: {
+//     // filteredRows: function () {
+//     //   var filter_name = this.filter_name.toLowerCase();
+//     //   return (this.filter_name.trim() !== '') ?
+//     //     this.rows.filter(function (d) { return d.name.toLowerCase().indexOf(filter_name) > -1; }) :
+//     //     this.rows;
+//     // },
 
-    pageStart: function () {
-      return (this.currPage - 1) * this.countOfPage;
-    },
-    totalPage: function () {
-      return Math.ceil(this.usuarios.length / this.countOfPage);
-    }
-  },
-  methods: {
-    setPage(idx) {
-      if (idx <= 0 || idx > this.totalPage) {
-        return;
-      }
-      this.currPage = idx;
-    },
-    ExportExcel(type, fn, dl) {
-      var elt = this.$refs.exportable_table;
-      var wb = XLSX.utils.table_to_book(elt, { sheet: "Sheet JS" });
-      return dl ?
-        XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }) :
-        XLSX.writeFile(wb, fn || (("data" + '.' || 'SheetJSTableExport.') + (type || 'xlsx')));
-    },
-    cerrarSesion() {
-      // e.preventDefault();
-      auth.signOut().then(() => {
-        window.location.href = "index.html";
+//     pageStart: function () {
+//       return (this.currPage - 1) * this.countOfPage;
+//     },
+//     totalPage: function () {
+//       return Math.ceil(this.usuarios.length / this.countOfPage);
+//     }
+//   },
+//   methods: {
+//     setPage(idx) {
+//       if (idx <= 0 || idx > this.totalPage) {
+//         return;
+//       }
+//       this.currPage = idx;
+//     },
+//     ExportExcel(type, fn, dl) {
+//       var elt = this.$refs.exportable_table;
+//       var wb = XLSX.utils.table_to_book(elt, { sheet: "Sheet JS" });
+//       return dl ?
+//         XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }) :
+//         XLSX.writeFile(wb, fn || (("data" + '.' || 'SheetJSTableExport.') + (type || 'xlsx')));
+//     },
+//     cerrarSesion() {
+//       // e.preventDefault();
+//       auth.signOut().then(() => {
+//         window.location.href = "index.html";
 
-      });
-    },
-    obtenerDatos() {
-      const onGetDatos = (callback) => db.collection("informacion").onSnapshot(callback);
-      onGetDatos((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          let usuario = doc.data();
-          usuario.id = doc.id;
-          this.usuarios.push(usuario);
-        });
-      });
+//       });
+//     },
+//     obtenerDatos() {
+//       const onGetDatos = (callback) => db.collection("informacion").onSnapshot(callback);
+//       onGetDatos((querySnapshot) => {
+//         querySnapshot.forEach((doc) => {
+//           let usuario = doc.data();
+//           usuario.id = doc.id;
+//           this.usuarios.push(usuario);
+//         });
+//       });
 
-    },
+//     },
 
-  },
-  mounted() {
-    this.obtenerDatos();
-  },
-}).$mount('#app')
+//   },
+//   mounted() {
+//     this.obtenerDatos();
+//   },
+// }).$mount('#app')
--- js/login.js
+++ js/login.js
@@ -1,23 +1,23 @@
 
 
 
-const formLogin = document.getElementById('form-login');
-const auth = firebase.auth();
-formLogin.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const email = formLogin["login-email"].value;
-  const password = formLogin["login-password"].value;
+// const formLogin = document.getElementById('form-login');
+// const auth = firebase.auth();
+// formLogin.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   const email = formLogin["login-email"].value;
+//   const password = formLogin["login-password"].value;
 
-  // Authenticate the User
-  auth.signInWithEmailAndPassword( email, password).then((userCredential) => {
-    // clear the form
-    // signInForm.reset();
-    // close the modal
-    window.location.href = "data.html";
-  })
-  .catch((error) => {
-    console.log(  error.code ,error.message  )
-    // const errorCode =
-    // const errorMessage = ;
-  });
-});
+//   // Authenticate the User
+//   auth.signInWithEmailAndPassword( email, password).then((userCredential) => {
+//     // clear the form
+//     // signInForm.reset();
+//     // close the modal
+//     window.location.href = "data.html";
+//   })
+//   .catch((error) => {
+//     console.log(  error.code ,error.message  )
+//     // const errorCode =
+//     // const errorMessage = ;
+//   });
+// });
