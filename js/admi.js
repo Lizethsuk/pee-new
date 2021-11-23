146e52d6a77e63d7821569a0d2b1b930853c9025
Sandra <sandrarocio12oc@gmail.com>
2021-11-23 12:27:04 -0500

    add areas

--- js/admi.js
+++ js/admi.js
@@ -25,6 +25,7 @@ function appendData(data) {
   title2Text.className = "col "
   title2Text.innerHTML= '<p class="title-text-curso">' + data.titleSecondCurso + '</p>'
   title2.appendChild(title2Text);
+  
   for (let i = 0; i < data.cursos.length; i++) {
     var mainContainer = document.getElementById("curso-content");
     console.log(data.cursos[i].section);  
@@ -58,14 +59,14 @@ function appendData(data) {
     var div = document.createElement("li");
     div.className = "curso-content-item li-area "
     div.innerHTML =
-      '<a target="_blank" href=  ' + data.cursos[i].linkCurso + ' class="text">'
-      + '<div style="border-color: '+ data.cursos[i].bgColorCursos +' "  class="box-area">'
-      + '<p class="title-curso">' + data.cursos[i].section + '</p>'
-      + '<p class="name-curso">' + data.cursos[i].curso + '</p>'
-      + '<hr style="border-color: '+ data.cursos[i].lineColorCursos +' " class="new4">' + '</hr>'
-      + '<img src=' + data.cursos[i].img + '  alt="">'
-      + '<p class="encargado">' + data.cursos[i].teacher + '</p>'
-      + '<p class="fecha">' + data.cursos[i].horario + '</p>'
+      '<a target="_blank" href=  ' + data.cursosSecond[i].linkCurso + ' class="text">'
+      + '<div style="border-color: '+ data.cursosSecond[i].bgColorCursos +' "  class="box-area">'
+      + '<p class="title-curso">' + data.cursosSecond[i].section + '</p>'
+      + '<p class="name-curso">' + data.cursosSecond[i].curso + '</p>'
+      + '<hr style="border-color: '+ data.cursosSecond[i].lineColorCursos +' " class="new4">' + '</hr>'
+      + '<img src=' + data.cursosSecond[i].img + '  alt="">'
+      + '<p class="encargado">' + data.cursosSecond[i].teacher + '</p>'
+      + '<p class="fecha">' + data.cursosSecond[i].horario + '</p>'
       + '</div>'
       + '</a>'
     mainContainer.appendChild(div);
